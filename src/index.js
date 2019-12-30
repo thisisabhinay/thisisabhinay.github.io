3dc9fae5092f12fc07cd8263a7cae0390742b956
thisisabhinay <a8h1nay@gmail.com>
2019-12-30 13:09:45 +0530

    Initialized react project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+    return(
+        <>
+            <h1>Under Construction</h1>
+        </>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
