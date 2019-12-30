31d4037f0116bace26aa744b66d14d2693cd1b01
thisisabhinay <a8h1nay@gmail.com>
2019-12-30 14:27:28 +0530

    Updates

---
+++ precache-manifest.b3b199a0df8f3bc1f87bc871983a7918.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fa8676da18a01055324d96206fd96c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "067f1ef3ed46087424ae",
+    "url": "/static/css/main.f6ab9422.chunk.css"
+  },
+  {
+    "revision": "fc626db099ccb82390bf",
+    "url": "/static/js/2.5c2fdc0c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5c2fdc0c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "067f1ef3ed46087424ae",
+    "url": "/static/js/main.840e4b95.chunk.js"
+  },
+  {
+    "revision": "6e0a713323cecb3a91e4",
+    "url": "/static/js/runtime-main.c93dd7d8.js"
+  }
+]);
